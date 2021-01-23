c77bc4b46953332a1e619fc67572d3d1443a74b0
benjamin hon <benjaminhon86@gmail.com>
2021-01-23 17:45:24 +0800

    skeleton

---
+++ setup.py
@@ -0,0 +1,32 @@
+from distutils.core import setup
+from antigen import __version__
+from setuptools import find_packages
+
+setup(
+    name="antigen",
+    version=__version__,
+    author="shirecoding",
+    author_email="shirecoding@gmail.com",
+    scripts=["bin/antigen"],
+    install_requires=["Jinja2"],
+    url="https://github.com/shirecoding/Antigen",
+    download_url=f"https://github.com/shirecoding/Antigen/archive/{__version__}.tar.gz",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    zip_safe=False,
+    packages=find_packages() + ["antigen.resources"],
+    package_data={
+        "antigen.resources": [
+            "templates/*",
+            "templates/**/*",
+            "templates/**/**/*",
+            "templates/**/**/**/*",
+            "templates/**/**/**/**/*",
+        ]
+    },
+)
